41efdb280bd93fc2f7f299dfa1cd019ad1b6e37f
Andrey Kandyba <andreikandyba@gmail.com>
2019-02-08 13:06:27 +0200

    Start project

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Andrey Kandyba on 08/02/2019.
+//  Copyright © 2019 ankaStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
