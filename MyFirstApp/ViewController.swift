912ad2bae8f0611574590b60ac304ac109825391
Andrey Kandyba <andreikandyba@gmail.com>
2019-02-08 20:22:59 +0200

    new changes in UI

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewOut: UILabel!
+    
+    
+    @IBAction func puchChange(_ sender: Any) {
+        viewOut.text = "Hello Swift!!!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
